3e3e7611a8d8fb99a3a3294c106fd76f46c20324
sasuniii0 <sasuniwijerathne@gmail.com>
2026-04-09 14:35:55 +0530

    feat - implemented all endpoints

--- wallet-service/src/main/java/com/example/wallet_service/config/SecurityConfig.java
+++ wallet-service/src/main/java/com/example/wallet_service/config/SecurityConfig.java
@@ -5,13 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,8 +19,6 @@ import java.util.List;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
-    private final UserDetailsServiceImpl userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
--- wallet-service/src/main/java/com/example/wallet_service/service/AccountService.java
+++ wallet-service/src/main/java/com/example/wallet_service/service/AccountService.java
@@ -5,7 +5,6 @@ import com.example.wallet_service.entity.Wallet;
 import com.example.wallet_service.repository.AccountRepository;
 import com.example.wallet_service.repository.WalletRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
